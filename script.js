398df95883a199357d6e472ac145e93a2ed42a16
LaisNardes <lais.nardes0398@gmail.com>
2024-06-06 22:38:21 -0400

    Projeto -Palindromo-Completo

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
-function isPalindrome(str){
-    /* Seu código aqui */
+function isPalindrome(str) {
+  const padraoLetras = str.replace(/[^A-Za-z0-9]/g, "").toLowerCase();
+  const comprimento = padraoLetras.length;
+
+  for (let i = 0; i < comprimento / 2; i++) {
+    if (padraoLetras[i] !== padraoLetras[comprimento - 1 - i]) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
-function arrayMaxMin(arr){
-    /* Seu código aqui */
+function arrayMaxMin(arr) {
+  let min = arr[0];
+  let max = arr[0];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+
+  return [min, max];
 }
